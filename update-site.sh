aea510b7f3d690a2b00b3b2a9d79b1417ba12eec
Antonio Cucchiara <antonio_cuc@yahoo.co.uk>
2018-10-23 14:03:07 +0100

    Debug automated gh-pages updates

--- update-site.sh
+++ update-site.sh
@@ -5,16 +5,17 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; th
     echo -e "Updating website..."
 
     cd $HOME
+    ls -ltrh
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "travis-ci"
-    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/AntoCuc/camunda-dmn-test-coverage gh-pages > /dev/null
+    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/AntoCuc/camunda-dmn-test-coverage gh-pages
 
     cd gh-pages
     git rm -rf ./*
     cp -Rf $HOME/target/site ./
     git add -f .
     git commit -m "Updated site for build $TRAVIS_BUILD_NUMBER"
-    git push -fq origin gh-pages > /dev/null
+    git push -fq origin gh-pages
 
     echo -e "Done.\n"
 
