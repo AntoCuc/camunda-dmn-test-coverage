0cd50d296ece6a99cc9174af0a433c7c51e183e1
Antonio Cucchiara <antonio_cuc@yahoo.co.uk>
2018-10-23 14:18:02 +0100

    Move artefacts before copying them.

--- update-site.sh
+++ update-site.sh
@@ -4,15 +4,16 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; th
 
     echo -e "Updating website..."
 
+    cp -R build/target/site $HOME/site
     cd $HOME
-    ls -ltrh
+
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "travis-ci"
     git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/AntoCuc/camunda-dmn-test-coverage gh-pages
 
     cd gh-pages
-    git rm -rf ./*
-    cp -Rf $HOME/target/site ./
+    git rm -rf ./site
+    cp -Rf $HOME/site ./
     git add -f .
     git commit -m "Updated site for build $TRAVIS_BUILD_NUMBER"
     git push -fq origin gh-pages
