58a0397eb5a7a85b969779d3240ee5698d633df2
Antonio Cucchiara <antonio_cuc@yahoo.co.uk>
2018-10-23 14:24:32 +0100

    Use the new $TRAVIS_BUILD_DIR rather than $HOME

--- update-site.sh
+++ update-site.sh
@@ -4,16 +4,13 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; th
 
     echo -e "Updating website..."
 
-    cp -R build/target/site $HOME/site
-    cd $HOME
-
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "travis-ci"
     git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/AntoCuc/camunda-dmn-test-coverage gh-pages
 
     cd gh-pages
-    git rm -rf ./site
-    cp -Rf $HOME/site ./
+    git rm -rf ./*
+    cp -Rf $TRAVIS_BUILD_DIR/target/site .
     git add -f .
     git commit -m "Updated site for build $TRAVIS_BUILD_NUMBER"
     git push -fq origin gh-pages
